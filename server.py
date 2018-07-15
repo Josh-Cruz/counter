1fce30d152612e686ba2ab34c7a4a715a09b63e7
Josh-Cruz <josh@digitalmonestary.com>
2018-07-15 15:38:35 -0700

    finished and did a bit of refactoring

--- server.py
+++ server.py
@@ -4,23 +4,19 @@ app.secret_key = 'meddlingKids'
 
 @app.route('/', methods=['GET', 'POST'])
 def index_counter():
-    if not session['count']:
+    if 'count' not in session:
         session['count'] = 1
     if request.method == 'POST':
         if request.form['update'] == 'plus_one':
             session['count'] += 1 
             print session['count']
-            return render_template ('index.html', count=session['count'])
         elif request.form['update'] == 'plus_two':
             session['count'] += 2
             print session['count']
-            return render_template ('index.html', count=session['count'])
         elif request.form['update'] == 'reset':
             session['count'] = 0
             print session['count']
-            return render_template ('index.html', count=session['count'])
-        else:
-            pass  
+        return render_template('index.html', count=session['count'])
     elif request.method == 'GET':
         return render_template ('index.html', count = session['count'])
 
